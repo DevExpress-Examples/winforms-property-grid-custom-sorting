5a9e28712cff80867c482780d5d58a3b61f0b397
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace PropertySorting {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.propertyGridControl1 = new DevExpress.XtraVerticalGrid.PropertyGridControl();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.propertyGridControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // propertyGridControl1
+            // 
+            this.propertyGridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propertyGridControl1.Location = new System.Drawing.Point(0, 0);
+            this.propertyGridControl1.Name = "propertyGridControl1";
+            this.propertyGridControl1.Size = new System.Drawing.Size(627, 490);
+            this.propertyGridControl1.TabIndex = 0;
+            this.propertyGridControl1.CustomPropertyDescriptors += new DevExpress.XtraVerticalGrid.Events.CustomPropertyDescriptorsEventHandler(this.propertyGridControl1_CustomPropertyDescriptors);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 446);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Toggle Sorting";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 490);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.propertyGridControl1);
+            this.Name = "Form1";
+            this.Text = "Properties\' Sorting";
+            ((System.ComponentModel.ISupportInitialize)(this.propertyGridControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraVerticalGrid.PropertyGridControl propertyGridControl1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CS/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraVerticalGrid.Events;
+using System.Collections;
+using System.Globalization;
+
+namespace PropertySorting {
+    public partial class Form1 : Form {
+        bool allowCustomSorting;
+
+        public Form1() {
+            InitializeComponent();
+            this.propertyGridControl1.SelectedObject = new CustomClass() { Property1 = "one", Property2 = "two", Property3 = "three", Property4 = "four", Property5 = "five" };
+        }
+
+        void propertyGridControl1_CustomPropertyDescriptors(object sender, CustomPropertyDescriptorsEventArgs e) {
+            if(allowCustomSorting && e.Context.PropertyDescriptor == null) {
+                e.Properties = e.Properties.Sort(new string[] { "Property5", "Property4", "Property3", "Property2", "Property1" });
+            }
+        }
+        void button1_Click(object sender, EventArgs e) {
+            allowCustomSorting = !allowCustomSorting;
+            propertyGridControl1.Refresh();
+            propertyGridControl1.RetrieveFields();
+        }
+    }
+    class CustomClass {
+        [DisplayName("C")]
+        public string Property1 { get; set; }
+        [DisplayName("B")]
+        public string Property2 { get; set; }
+        [DisplayName("A")]
+        public string Property3 { get; set; }
+        [DisplayName("D")]
+        public string Property4 { get; set; }
+        [DisplayName("E")]
+        public string Property5 { get; set; }
+    }
+}
